86f525a5a579afa03adb2ecad1ad4909c2207053
ismae <ismaeltoledodemedeiros@gmail.com>
2021-06-12 10:58:23 -0300

    Adicionando arquivos

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+static class Constante
+{
+    public const double discount = 5;
+}
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public string name { get; set; }
+        public int NumberAccount { get; private set; }
+        private double depositAmout;
+        private double accountBalance;
+        private double discount;
+
+        public Conta(int number_accout, string name_user, double init_value)
+        {
+            name = name_user;
+            NumberAccount = number_accout;
+            accountBalance = init_value;
+        }
+        public Conta(int number_accout, string name_user)
+        {
+            name = name_user;
+            NumberAccount = number_accout;
+        }
+        public Conta()
+        {
+        }
+
+        public double DepositAmout
+        {
+            get { return  depositAmout; }
+            set {  depositAmout = value; }
+        }
+
+
+        //metodo para deposito
+        public void Deposit (double deposit)
+        {
+            Console.WriteLine("O deposito foi no valor de " + deposit);
+            accountBalance += deposit;
+            Console.WriteLine("O saldo da conta e de " + accountBalance);
+        }
+
+        //metodo para saque 
+        public void withdraw (double valueWithdraw)
+        {
+            accountBalance = accountBalance - (discount + valueWithdraw);
+           
+        }
+
+        public override string ToString()
+        {
+       
+            return "o numero da conta"
+                + NumberAccount + "\n"
+                + "Nome do titular"
+                + name + "\n"
+                + "Saldo:"
+                + accountBalance;
+
+        }
+
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta;
+            Console.WriteLine("Digite o numero da conta:");
+            int numberAccout = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quem será responsavel pela conta:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Deseja fazer um deposito inicial?(S/N)");
+            char Resposta = char.Parse(Console.ReadLine());
+            if (Resposta == 'S'|| Resposta == 's')
+            {
+                Console.WriteLine("Qual e o valor que deseja depositar");
+                double depositInti = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numberAccout, name, depositInti);
+            }
+            else
+            {
+                conta = new Conta(numberAccout, name);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Faça um deposito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposit(valor);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Faça um saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.withdraw(valor);
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+        }
+    }
+}
